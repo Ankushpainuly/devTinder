3cc15106555cc529434be6778bba412594638c3f
Ankush Painuly <ankushpainuly01729@gmail.com>
2025-09-30 18:53:17 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -3,14 +3,28 @@ const express=require('express');
 
 const app=express();
 
+//order matters
 
-app.use("/contact",(req,res)=>{
-    res.send("This is the contact page!");
+
+//this will only handle get calls to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Ankush", lastName:"Painuly"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("data send sucessfully.");
 });
 
-app.use("/",(req,res)=>{
-    res.send("Home page!!");
+app.delete("/user",(req,res)=>{
+    res.send("data deleted sucessfully.");
 });
+
+//this will match all the HTTP methods APIs calls to /test
+app.use("/test",(req,res)=>{
+    res.send("This is the test page!");
+});
+
+
 
 app.listen(777,()=>{
     console.log("Server is sucessfully lisning on port 777...");
